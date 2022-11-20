f738d58ad54fff47459dd8fc4282f77e635f668a
ICTMubarak <engr.md.mubarak.hossain@gmail.com>
2022-11-19 17:53:50 -0800

    Modal add successfullay

--- src/Pages/Appointment/AbailableAppointment/AbailableAppointment.js
+++ src/Pages/Appointment/AbailableAppointment/AbailableAppointment.js
@@ -1,12 +1,30 @@
 import { format } from 'date-fns';
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import BookingModal from '../BookingModal/BookingModal';
+import AppointmentOptions from './AppointmentOptions';
 
 const AbailableAppointment = ({selectDate}) => {
+    const [appointmentOptions, setAppointmentOptions] = useState([])
+    useEffect(() =>{
+        fetch('appointmentOptions.json')
+        .then(res => res.json())
+        .then(data => setAppointmentOptions(data))
+    }, [])
     
+    //console.log(appointmentOptions);
     return (
-        <div>
+        <section>
             <p className='text-ceter text-primary'>Abailable appointment on {format(selectDate, 'PP')}</p>
-        </div>
+            <div className='grid gap-6 grid-cols-1 md:grid-cols-2 lg:grid-cols-3'>
+                {
+                    appointmentOptions.map(options => <AppointmentOptions
+                    key={options._id}
+                    appointmentOption = {options}
+                    ></AppointmentOptions>)
+                }
+            </div>
+            <BookingModal></BookingModal>
+        </section>
     );
 };
 
---
+++ src/Pages/Appointment/AbailableAppointment/AppointmentOptions.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const AppointmentOptions = ({appointmentOption}) => {
+const {name, slots } = appointmentOption;
+console.log(appointmentOption);
+    return (
+        <div className="card w-96 bg-base-100 shadow-xl">
+  <div className="card-body text-center">
+    <h2 className="card-title">{name}</h2>
+    <p>{slots.length > 0 ? slots[0] : 'Try another day'}</p>
+    <p>{slots.length} slots available</p>
+    <div className="card-actions">
+      <label htmlFor="booking-modal" className="btn btn-primary">Book Appointment</label>
+
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default AppointmentOptions;
